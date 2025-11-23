e707931c5d049d48a2806e8df4b3187150f3bb53
Eyedfish025 <jonatasgustavo24@gmail.com>
2025-11-23 17:14:30 -0300

    logout

--- SoundsGood/app/Http/Controllers/AuthController.php
+++ SoundsGood/app/Http/Controllers/AuthController.php
@@ -50,3 +50,9 @@ class AuthController extends Controller
         return redirect()-> route('/Atividades');
     }
 }
+    public function logout(Request $request){
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('SoundsGood');
+    }
